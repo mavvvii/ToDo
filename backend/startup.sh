74fd94bac541fc2cd4b1daaf7203e644140ecbc3
mavvvii <mavi.steampl@gmail.com>
2025-07-30 16:46:36 +0000

    feat: use giunicorn instead of uvicorn

--- backend/startup.sh
+++ backend/startup.sh
@@ -14,11 +14,11 @@ poetry run python manage.py createadmin
 echo "[ALERT] Collect static files..."
 poetry run python manage.py collectstatic --noinput
 
-if [ "$DJANGO_PRODUCTION" = "True" ]; then
+if [ "$DJANGO_PRODUCTION" = "1" ]; then
   echo "Running production server"
-  poetry run uvicorn backend.asgi:application --host 0.0.0.0 --port "8000" --log-level info --workers "4"
+  poetry run gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 4 --log-level info
 else
   echo "Running developing server"
-  # poetry run python manage.py runserver 0.0.0.0:8000
-  poetry run uvicorn backend.asgi:application --host 0.0.0.0 --port "8000" --reload --reload-dir /app
+  poetry run python manage.py runserver 0.0.0:8000
+  # poetry run gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --reload --log-level info
 fi
