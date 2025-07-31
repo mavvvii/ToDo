4af96cf55b97b5507add96b45d8f447970de03f6
mavvvii <mavi.steampl@gmail.com>
2025-07-31 20:42:30 +0000

    feat: import login serializer

--- backend/users/serializers/__init__.py
+++ backend/users/serializers/__init__.py
@@ -6,21 +6,22 @@ This module provides versioned serializers for the User model, used in different
 Exported Classes:
 - UserDetailSerializerV1: Serializer for user details in version 1 of the API.
 - RegisterSerializerV1: Serializer for user registration in version 1 of the API.
+- LoginSerializerV1: Serializer for user login in version 1 of the API.
 
 Typical usage example:
     from users.serializers import UserDetailSerializerV1
 
     serializer_class = UserDetailSerializerV1
-
-    -----------------------------------------------------
-    from users.serializers import RegisterSerializerV1
-
-    serializer_class = RegisterSerializerV1
 """
 
-from .v1 import RegisterSerializerV1, UserDetailSerializerV1
+from .v1 import (
+    LoginSerializerV1,
+    RegisterSerializerV1,
+    UserDetailSerializerV1,
+)
 
-__all__ = [
+__all__: list[str] = [
     "UserDetailSerializerV1",
     "RegisterSerializerV1",
+    "LoginSerializerV1",
 ]
