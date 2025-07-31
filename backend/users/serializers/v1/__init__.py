03d8acfe53c21019c0c572715aacdc6f4d318910
mavvvii <mavi.steampl@gmail.com>
2025-07-31 20:42:02 +0000

    feat: import login serializer

--- backend/users/serializers/v1/__init__.py
+++ backend/users/serializers/v1/__init__.py
@@ -3,12 +3,15 @@
 Exported Classes:
 - UserDetailSerializerV1: Serializer for user details in version 1 of the API.
 - RegisterSerializerV1: Serializer for user registration in version 1 of the API.
+- LoginSerializerV1: Serializer for user login in version 1 of the API.
 """
 
+from .login import LoginSerializer as LoginSerializerV1
 from .register import RegisterSerializer as RegisterSerializerV1
 from .user import UserDetailSerializer as UserDetailSerializerV1
 
-__all__ = [
+__all__: list[str] = [
     "UserDetailSerializerV1",
     "RegisterSerializerV1",
+    "LoginSerializerV1",
 ]
