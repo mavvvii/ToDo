25656aa19933e65400c059fe7b6c1113b339861a
mavvvii <mavi.steampl@gmail.com>
2025-08-06 20:55:43 +0000

    refactor: remove console logs

--- frontend/src/js/api/refresh-token.js
+++ frontend/src/js/api/refresh-token.js
@@ -1,7 +1,6 @@
 const API_V1_BASE_URL = 'http://localhost:8000/api/v1'
 
 export async function refreshToken() {
-    console.log('Calling refreshToken()');
     try {
         const response = await fetch(`${API_V1_BASE_URL}/token/refresh/`, {
             method: 'POST',
@@ -9,19 +8,15 @@ export async function refreshToken() {
                 'Content-Type': 'application/json',
             },
             credentials: 'include',
-            body: JSON.stringify({})  // Pusty body musi być
+            body: JSON.stringify({})
         });
-        console.log('refreshToken response status:', response.status);
         if (!response.ok) {
             const text = await response.text();
-            console.error('Token refresh failed:', text);
             throw new Error('Token refresh failed');
         }
         const data = await response.json();
-        console.log('refreshToken data:', data);
         return data;
     } catch (error) {
-        console.error('refreshToken error:', error);
         throw error;
     }
 }
