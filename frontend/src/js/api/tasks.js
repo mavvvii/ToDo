b95d33d191ad1d24ce4c069aaec612abf8f5efa7
mavvvii <mavi.steampl@gmail.com>
2025-08-09 21:30:42 +0000

    feat: handle response errors

--- frontend/src/js/api/tasks.js
+++ frontend/src/js/api/tasks.js
@@ -1,18 +1,11 @@
 import { refreshToken } from './refresh-token.js';
+import { getCsrfToken } from '../scripts/get-csrf-token.js';
 
 const API_V1_BASE_URL = 'http://localhost:8000/api/v1'
+const csrfToken = getCsrfToken();
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-    return null;
-}
-
-const csrfToken = getCookie('csrftoken');
-
-export async function getTasks(board_id) {
-    const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks`, {
+export async function getTasks(board_id, isRetry = false) {
+    const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -20,16 +13,21 @@ export async function getTasks(board_id) {
         credentials: 'include',
     });
 
-    if (response.status == 401) {
+    if (response.status == 401 && !isRetry) {
         await refreshToken();
-        return await getTasks(board_id);
+        return await getTasks(board_id, true);
+    }
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || 'Error during get tasks!');
     }
 
   return await response.json();
 }
 
-export async function getTask(board_id, task_id) {
-  const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/task/${task_id}`, {
+export async function getTask(board_id, task_id, isRetry = false) {
+  const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/${task_id}/`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -37,20 +35,20 @@ export async function getTask(board_id, task_id) {
     credentials: 'include',
   });
 
-  if (response.status === 401) {
+  if (response.status === 401 && !isRetry) {
     await refreshToken();
-    return await getTask(board_id);
+    return await getTask(board_id, task_id, true);
   }
 
   if (!response.ok) {
     const errorData = await response.json();
-    throw new Error(errorData.detail || 'Error during get tasks!');
+    throw new Error(errorData.detail || 'Error during get task!');
   }
 
   return await response.json();
 }
 
-export async function createTask(board_id, title, description) {
+export async function createTask(board_id, title, description, isRetry = false) {
   const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/`, {
     method: 'POST',
     headers: {
@@ -64,6 +62,11 @@ export async function createTask(board_id, title, description) {
     }),
   });
 
+  if (response.status === 401 && !isRetry) {
+    await refreshToken();
+    return await createTask(board_id, title, description, true);
+  }
+
   if (!response.ok) {
     const errorData = await response.json();
     throw new Error(errorData.detail || 'Error during create task!');
@@ -72,13 +75,12 @@ export async function createTask(board_id, title, description) {
   return await response.json();
 }
 
-export async function updateTask(board_id, task_id, title, description, status, completed) {
+export async function updateTask(board_id, task_id, title, description, status, completed, isRetry = false) {
     const data = {};
     if (title && title.trim() !== '') data.title = title;
     if (description && description.trim() !== '') data.description = description;
     if (status && status.trim() !== '') data.status = status;
     if (completed && completed.trim() !== '') data.completed = completed;
-
 
     const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/${task_id}/`, {
         method: 'PATCH',
@@ -92,17 +94,21 @@ export async function updateTask(board_id, task_id, title, description, status, 
         ),
     });
 
+    if (response.status === 401 && !isRetry) {
+      await refreshToken();
+      return await updateTask(board_id, task_id, title, description, status, completed, true);
+    }
 
     if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.detail || 'Error during update task!');
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Error during update task!');
     }
 
     return await response.json();
 }
 
-export async function deleteTask(board_id, task_id) {
-    const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/${task_id}`, {
+export async function deleteTask(board_id, task_id, isRetry = false) {
+    const response = await fetch(`${API_V1_BASE_URL}/boards/${board_id}/tasks/${task_id}/`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
@@ -111,11 +117,15 @@ export async function deleteTask(board_id, task_id) {
         credentials: 'include',
     });
 
+    if (response.status === 401 && !isRetry) {
+        await refreshToken();
+        return await deleteTask(board_id, task_id, true);
+    }
 
     if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.detail || 'Error during delete task!');
     }
 
-    return true;
+    return await response.json();
 }
