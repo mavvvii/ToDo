367ff4349ead58237ad7fe854a8cceccea36bcb0
mavvvii <mavi.steampl@gmail.com>
2025-08-09 21:36:05 +0000

    feat: script go get csrf token

---
+++ frontend/src/js/scripts/get-csrf-token.js
@@ -0,0 +1,12 @@
+function getCsrfToken() {
+  const name = 'csrftoken=';
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookies = decodedCookie.split(';');
+  for (let cookie of cookies) {
+    cookie = cookie.trim();
+    if (cookie.startsWith(name)) {
+      return cookie.substring(name.length);
+    }
+  }
+  return null;
+}
